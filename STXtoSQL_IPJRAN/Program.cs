ebd1a84c08dec0bba4b70e166d6dad53d5ec607a
Scott Clemons <scott.clemons317@outlook.com>
2020-06-04 13:54:22 -0600

    Add project files.

---
+++ STXtoSQL_IPJRAN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace STXtoSQL_IPJRAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
